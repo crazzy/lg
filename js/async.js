25069e62f447fbf8e4728e87cad671b3c7a0c6d8
Johan Hedberg <mail@johan.pp.se>
2015-04-21 23:30:28 +0200

    Rewriting JS stuff to use setTimeout instead of a loop as JS is shitty and hangs the GUI otherwise

--- js/async.js
+++ js/async.js
@@ -1,10 +1,66 @@
 var lg_async_error = false;
 var lg_has_data = false;
 var lg_request_complete = false;
+var lg_async_id = null;
+var lg_async_nextchunk = null;
 function lg_async_handle_error(jqXHR, textStatus, errorThrown) {
 	window.lg_async_error = true;
 	alert(textStatus);
 	alert(errorThrown);
+}
+function lg_async_handler() {
+	$.ajax({
+		url: '/async.php',
+		method: 'POST',
+		data: {
+			async_id: window.lg_async_id,
+			nextchunk: window.lg_async_nextchunk
+		},
+		error: lg_async_handle_error,
+		success: function(data, textStatus, jqXHR) {
+			if(data == "error") {
+				window.lg_async_error = true;
+				return false;
+			}
+			if(data == "init") {
+				return false;
+			}
+			if(data == "wait") {
+				return false;
+			}
+			if(data.trim() == "") {
+				return false;
+			}
+			jqXHR.getAllResponseHeaders(); // For some odd fucking reason this needs to be called before checking invidual headers - otherwise they're not found....
+			if(jqXHR.getResponseHeader('X-LG-Async-Status') == 'complete') {
+				window.lg_request_complete = true;
+			}
+			if(window.lg_has_data === false) {
+				window.lg_has_data = true;
+				$("form").after("<pre>" + data + "</pre>");
+			}
+			else {
+				$("pre").html($("pre").html() + data);
+			}
+		}
+	});
+	if(window.lg_async_error) {
+		return false;
+	}
+	if(window.lg_has_data === false) {
+		setTimeout(lg_async_handler, 500);
+		return true;
+	}
+	if(window.lg_request_complete === true) { // For extra safety resetting everything now
+		window.lg_async_error = false;
+		window.lg_has_data = false;
+		window.lg_request_complete = false;
+		window.lg_async_id = null;
+		window.lg_async_nextchunk = null;
+		return true;
+	}
+	window.lg_async_nextchunk += 1;
+	setTimeout(lg_async_handler, 500);
 }
 $(document).ready(function() {
 	$("form").each(function(index, value) {
@@ -21,55 +77,16 @@ $(document).ready(function() {
 					async: 'true'
 				},
 				success: function(async_id, textStatus, jqXHR) {
-					var nextchunk = 0;
+					window.lg_async_nextchunk = 0;
 					async_id = async_id.toString().trim();
 					if(async_id == "") {
 						alert("errar!");
 					}
-					do {
-						window.lg_has_data = false;
-						window.lg_request_complete = false;
-						$.ajax({
-							url: '/async.php',
-							method: 'POST',
-							data: {
-								async_id: async_id,
-								nextchunk: nextchunk
-							},
-							error: lg_async_handle_error,
-							success: function(data, textStatus, jqXHR) {
-								if(data == "error") {
-									window.lg_async_error = true;
-									return false;
-								}
-								if(data == "init") {
-									return false;
-								}
-								if(data == "wait") {
-									return false;
-								}
-								if(jqXHR.getResponseHeader('X-LG-Async-Status') == 'complete') {
-									window.lg_request_complete = true;
-								}
-								if(!window.lg_has_data) {
-									$("form").after("<pre>" + data + "</pre>");
-								}
-								window.lg_has_data = true;
-								$("pre").html($("pre").html() + data);
-							}
-						});
-						if(window.lg_async_error) {
-							break;
-						}
-						if(!window.lg_has_data) {
-							continue;
-						}
-						if(window.lg_request_complete) {
-							break;
-						}
-						nextchunk += 1;
-					} while(true);
-					return true;
+					window.lg_async_id = async_id;
+					window.lg_has_data = false;
+					window.lg_request_complete = false;
+					setTimeout(lg_async_handler, 500);
+					return false;
 				},
 				error: lg_async_handle_error
 			});
