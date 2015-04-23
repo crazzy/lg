038974b28ded63ccaaf1f7775f7ed3106c50714c
Johan Hedberg <mail@johan.pp.se>
2015-04-23 23:54:53 +0200

    Updating example-configuration

--- config-sample.php
+++ config-sample.php
@@ -10,14 +10,14 @@
 $global_config = array(
 	'companyname' => 'MyCompany',
 	'companylink' => 'http://example.org',
-	'companyname' => '',
+	'companylogo' => '', # URL for img-tag
 	'theme' => 'basic',
 	'memcache_host' => '127.0.0.1',
 	'memcache_port' => '11211',
 	'memcache_prefix' => 'lg',
 	'ratelimit_global' => 60, # Per minute
-	'ratelimit_perip' => 5,
-	'ratelimit_whitelist' => array(),
+	'ratelimit_perip' => 5, # Per minute
+	'ratelimit_whitelist' => array(), # List of v4/v6 ip's/cidr's as strings
 	'checks_enabled' => array('ping', 'traceroute', 'dns', 'bgp')
 );
 $routers = array(
@@ -31,7 +31,15 @@ $routers = array(
 	),
 	'rtr2' => array(
 		'address' => '10.0.0.2',
-		'plugin' => 'ssh_openbgpd',
+		'plugin' => 'ssh_cisco',
+		'pluginparams' => array(
+			'ssh_username' => 'secret',
+			'ssh_password' => 'secret'
+		)
+	),
+	'rtr3' => array(
+		'address' => '10.0.0.3',
+		'plugin' => 'ssh_quagga',
 		'pluginparams' => array(
 			'ssh_username' => 'secret',
 			'ssh_password' => 'secret'
